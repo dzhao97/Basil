212c5b5a8e6956487db126aa98d5755466b2b503
Dennis Zhao <denniszhao@Denniss-MacBook-Pro-4.local>
2019-07-22 18:42:44 -0400

    See previous commit message

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 
+const { getDates } = require('./lib/dateRange')
 const { fetchTransactions } = require('./lib/fetch')
 const { transformTransactionsToUpdates } = require('./lib/transform')
 const { updateSheet } = require('./lib/update')
 
 ;(async () => {
-  const transactions = await fetchTransactions()
+  const dates = await getDates()
+  const transactions = await fetchTransactions(dates[0], dates[1])
   const updates = transformTransactionsToUpdates(transactions)
   updateSheet(updates)
 })()
