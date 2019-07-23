e8042814da0815d4592991840dc610196a18105b
Dennis Zhao <denniszhao@Denniss-MacBook-Pro-4.local>
2019-07-23 00:26:01 -0400

    Add summary tables

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ const { updateSheet } = require('./lib/update')
 ;(async () => {
   const dates = await getDates()
   const transactions = await fetchTransactions(dates[0], dates[1])
-  const updates = transformTransactionsToUpdates(transactions)
-  updateSheet(updates)
+  const updates_bound = transformTransactionsToUpdates(transactions)
+  updateSheet(updates_bound[0], updates_bound[1])
 })()
