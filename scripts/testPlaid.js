d7c60c950772527f775273d9800b52b16821e705
Dennis Zhao <denniszhao@Denniss-MacBook-Pro-4.local>
2019-09-29 01:21:38 -0700

    fix plaid test

--- scripts/testPlaid.js
+++ scripts/testPlaid.js
@@ -2,10 +2,12 @@ require('dotenv').config()
 
 const path = require('path')
 const { writeFile } = require('fs-extra')
+const { getDates } = require('../lib/dateRange')
 const { fetchTransactions } = require('../lib/fetch')
 
 ;(async () => {
-  const res = await fetchTransactions()
+  const dates = await getDates()
+  const res = await fetchTransactions(dates[0], dates[1])
   console.log('Transactions fetch successful!')
   console.log(res)
 })()
